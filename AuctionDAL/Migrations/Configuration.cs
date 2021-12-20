88136a0e6813f28fd3b9d3d544312e3f1a9a7e2c
t0pez <reloader33el@gmail.com>
2021-12-20 23:32:00 +0200

    First migration

---
+++ AuctionDAL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace AuctionDAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AuctionDAL.Context.AuctionContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(AuctionDAL.Context.AuctionContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
